346b6a5d0b6ac605a27838e44263706cd7d2d1db
Mahir Musayev <146106439+musayevmahir68@users.noreply.github.com>
2024-03-28 00:11:36 +0400

    class task

---
+++ Inheritence/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Inheritence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student("mahir" ,"musayev","AB106" ,99.9,true);
+            student1.GetInfo();
+            student1.CheckGraduation();
+            student1.GetDiplomDegree();
+        }
+
+    }
+}
---
+++ Inheritence/Student.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritence
+{
+    internal class Student
+    {
+        public string Name;
+        public string Surname;
+        public string Group;
+        public double Point;
+        public bool IsGraduated;
+        public Student(string name, string surname, string group, double point, bool isGraduated)
+        {
+            Name = name;
+            Surname = surname;
+            Group = group;
+            Point = point;
+            IsGraduated = point >= 65;
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine($"{Name} \n{Surname} \n{Group} \n{Point} \n{IsGraduated}");
+        }
+        public void CheckGraduation()
+        {
+            if (IsGraduated)
+            {
+                Console.WriteLine("mezun olub");
+            }
+            else
+            {
+                Console.WriteLine("mezun olmayib");
+            }
+        }
+        public void GetDiplomDegree()
+        {
+            if (Point <65)
+            {
+                Console.WriteLine("diplom yoxdur");
+            }
+            else if (Point >=65 &&  Point < 80)
+            {
+                Console.WriteLine("adi diplom");
+            }
+            else
+            {
+                Console.WriteLine("yuksek seref diplomu");
+            }
+        }
+    }
+}
+
